61cc86519d63986939690d5abecca251e3eb9913
Patrick Boyd <pboyd04@gmail.com>
2022-03-30 14:40:52 -0500

    Add ability to get channel names from the control radio/repeater

---
+++ Moto.Net/Mototrbo/XNL/XCMP/CloneReadReply.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moto.Net.Mototrbo.XNL.XCMP
+{
+    public class CloneReadReply : XCMPPacket
+    {
+        protected UInt16 zone;
+        protected UInt16 channel;
+        protected UInt16 dataType;
+
+        public CloneReadReply() : base(XCMPOpCode.CloneReadReply)
+        {
+
+        }
+
+        public CloneReadReply(byte[] data) : base(data)
+        {
+            //data[3-4] = 0x8001 - I think this basically asks for data about zone in the next word
+            zone = (UInt16)(data[5] << 8 | data[6]);
+            //data[7-8] = 0x8002 - I think this basically asks for data about channel in the next word
+            channel = (UInt16)(data[9] << 8 | data[10]);
+            dataType = (UInt16)(data[11] << 8 | data[12]);
+            //Next two bytes are the length, just skip that... (would need if I were parsing responses with multiple data bits in them)
+            this.data = data.Skip(15).ToArray();
+        }
+    }
+}
---
+++ Moto.Net/Mototrbo/XNL/XCMP/CloneReadRequest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moto.Net.Mototrbo.XNL.XCMP
+{
+    public class CloneReadRequest : XCMPPacket
+    {
+        public CloneReadRequest(UInt16 zone, UInt16 channel, byte dataType) : base(XCMPOpCode.CloneReadRequest)
+        {
+            //If I'm interpretting things right then RDAC asks for a couple of things at once. I'm just going to ask for one thing at once to simplify things for now
+            this.data = new byte[10];
+            this.data[0] = 0x80;
+            this.data[1] = 0x01;
+            this.data[2] = (byte)(zone >> 8);
+            this.data[3] = (byte)zone;
+            this.data[4] = 0x80;
+            this.data[5] = 0x02;
+            this.data[6] = (byte)(channel >> 8);
+            this.data[7] = (byte)channel;
+            this.data[8] = 0x00;
+            this.data[9] = dataType; //0x0F is the channel name
+        }
+    }
+}
