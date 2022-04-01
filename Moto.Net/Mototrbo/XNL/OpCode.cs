8e6586fd81d3ebd26541f3a400d8b17dcd0641e9
Patrick Boyd <pboyd04@gmail.com>
2022-04-01 09:28:36 -0500

    Add DeviceSysMapRequest OpCode

--- Moto.Net/Mototrbo/XNL/OpCode.cs
+++ Moto.Net/Mototrbo/XNL/OpCode.cs
@@ -8,6 +8,7 @@
         DeviceAuthKeyReply = 0x05,
         DeviceConnectionRequest = 0x06,
         DeviceConnectionReply = 0x07,
+        DeviceSysMapRequest = 0x08, //This just kicks off a new DeviceSysMapBroadcast from the connected radio. I haven't found a need for this yet.
         DeviceSysMapBroadcast = 0x09,
         DataMessage = 0x0b,
         DataMessageAck = 0x0c
