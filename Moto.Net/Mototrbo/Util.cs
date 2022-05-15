d526c83bb8bcb99a398aa2c846007d8512a444b2
Patrick Boyd <pboyd04@gmail.com>
2022-05-15 12:49:10 -0500

    Handle bad RSSI data correctly

--- Moto.Net/Mototrbo/Util.cs
+++ Moto.Net/Mototrbo/Util.cs
@@ -10,6 +10,10 @@ namespace Moto.Net.Mototrbo
     {
         public static float CalcRSSI(byte[] data, int offset)
         {
+            if(data[offset] == 0 && data[offset + 1] == 0)
+            {
+                return -1;
+            }
             return (float)(-1.0 * (float)data[offset] + (data[offset + 1] * 1000.0 + 128.0) / 256000.0);
         }
     }
