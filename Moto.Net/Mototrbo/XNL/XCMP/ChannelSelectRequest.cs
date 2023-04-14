06469f00149e1b6568f3a816eaa19656cc38e332
Patrick Boyd <pboyd04@gmail.com>
2023-04-14 08:51:52 -0500

    Add new ChannelSelectFunction types from playing with the radio

--- Moto.Net/Mototrbo/XNL/XCMP/ChannelSelectRequest.cs
+++ Moto.Net/Mototrbo/XNL/XCMP/ChannelSelectRequest.cs
@@ -8,6 +8,14 @@ namespace Moto.Net.Mototrbo.XNL.XCMP
 {
     public enum ChannelSelectFunction
     {
+        NextZone = 0x01, //zone and channel don't seem to matter. Gets the radio to go to the next zone
+        PrevousZone = 0x02, //zone and channel don't seem to matter. Gets the radio to go to the previous zone
+        NextChannel = 0x03, //zone and channel don't seem to matter. Gets the radio to go to the next channel
+        PreviousChannel = 0x04, //zone and channel don't seem to matter. Gets the radio to go to the previous channel
+        SetChannelAndZone = 0x05, //Sets the zone and channel on the radio to the one specified
+        SetChannel = 0x06, //Set just the channel on the radio, zone is ignored
+        SetZone = 0x07, //Set just the zone on the radio, channel is ignored
+        //0x08 seems to always return BadParams same for other numbers I've tried
         GetCurrentZoneNumber = 0x80, //Zone will contain the current zone id
         GetZoneCount = 0x81, //Zone will contain the number of zones (+1 in my config I think for the channel pool?)
         GetChannelCount = 0x82, //Channel will contain the channel count for the requested zone
